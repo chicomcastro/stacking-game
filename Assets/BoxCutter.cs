67eb891aa2af3e014a5af90dccf1c8be2a9775e4
Francisco Castro <francisco.castro@sooper.com.br>
2022-05-03 22:03:11 -0300

    Version 2 of BoxCutter

--- Assets/BoxCutter.cs
+++ Assets/BoxCutter.cs
@@ -4,38 +4,75 @@ using UnityEngine;
 
 public class BoxCutter : MonoBehaviour
 {
+    public GameObject boxPrefab;
+    public GameObject upperBox;
+    public GameObject baseBox;
+
+    private void Start()
+    {
+        CutBoxes(upperBox, baseBox);
+    }
+
     public Vector3[] CutBoxes(GameObject upperBox, GameObject baseBox)
     {
-        Vector3[] P = GetBoundaries(upperBox);
-        Vector3[] Q = GetBoundaries(baseBox);
+        Vector3[] P = GetBoundaries(baseBox);
+        Vector3[] Q = GetBoundaries(upperBox);
 
-        Vector3[] A1 = MountBox(P[3], Q[1]);
-        print(A1[0]);
-        print(A1[1]);
-        print(A1[2]);
-        print(A1[3]);
-        return A1;
+        Vector3[] R = MountBox(P[0], P[1], Q[1], Q[2]);
+        InstantiateBox(R, upperBox);
+
+        print(R[0]);
+        print(R[1]);
+        print(R[2]);
+        print(R[3]);
+
+        return R;
     }
 
     private Vector3[] GetBoundaries(GameObject box)
     {
         Bounds bounds = box.GetComponent<BoxCollider>().bounds;
         Vector3 center = bounds.center;
-        Vector3 P1 = center + bounds.extents.x * Vector3.right;
-        Vector3 P2 = center + bounds.extents.z * Vector3.forward;
-        Vector3 P3 = center - bounds.extents.x * Vector3.right;
-        Vector3 P4 = center - bounds.extents.z * Vector3.forward;
+
+        Vector3 x = bounds.extents.x * transform.right;
+        Vector3 z = bounds.extents.z * transform.forward;
+
+        Vector3 diag1 = z + x;
+        Vector3 diag2 = z - x;
+
+        Vector3 P1 = center + diag1;
+        Vector3 P2 = center + diag2;
+        Vector3 P3 = center - diag1;
+        Vector3 P4 = center - diag2;
+
         return new Vector3[] { P1, P2, P3, P4 };
     }
 
-    private Vector3[] MountBox(Vector3 P, Vector3 Q)
+    private Vector3[] MountBox(Vector3 P1, Vector3 P2, Vector3 P3, Vector3 P4)
     {
-        Vector3 center = (P + Q) / 2;
-        Vector3 diag = P - Q;
-        Vector3 P1 = center + Quaternion.Euler(0, 0, 0) * diag / 2;
-        Vector3 P2 = center + Quaternion.Euler(0, 90, 0) * diag / 2;
-        Vector3 P3 = center + Quaternion.Euler(0, 180, 0) * diag / 2;
-        Vector3 P4 = center + Quaternion.Euler(0, 270, 0) * diag / 2;
-        return new Vector3[] { P1, P2, P3, P4 };
+        Vector3 center = (P1 + P3) / 2;
+        Vector3 diag1 = (P1 - P3) / 2;
+        Vector3 diag2 = (P2 - P4) / 2;
+
+        Vector3 Q1 = center + diag1;
+        Vector3 Q2 = center + diag2;
+        Vector3 Q3 = center - diag1;
+        Vector3 Q4 = center - diag2;
+
+        return new Vector3[] { Q1, Q2, Q3, Q4 };
+    }
+
+    private void InstantiateBox(Vector3[] box, GameObject refBox)
+    {
+        print(box[0]);
+        Vector3 boxCenter = (box[0] + box[2]) / 2;
+        GameObject gamo = Instantiate(boxPrefab, boxCenter, refBox.transform.rotation);
+
+        Vector3 desiredSize = new Vector3((box[0] - box[1]).magnitude, 1, (box[0] - box[3]).magnitude);
+        gamo.transform.localScale = new Vector3(
+            gamo.GetComponent<BoxCollider>().size.x / desiredSize.x * gamo.transform.localScale.x,
+            gamo.GetComponent<BoxCollider>().size.y / desiredSize.y * gamo.transform.localScale.y,
+            gamo.GetComponent<BoxCollider>().size.z / desiredSize.z * gamo.transform.localScale.z
+        );
     }
 }
