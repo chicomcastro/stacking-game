1e732fa1d16c3fe071e28175628d6cd78a5ac5dd
Francisco Castro <francisco.castro@sooper.com.br>
2022-05-14 16:17:40 -0300

    feat: scale correction based on last spawned box final size

--- Assets/Spawner.cs
+++ Assets/Spawner.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,6 +15,8 @@ public class Spawner : MonoBehaviour
 
     private LinkedList<Transform> spawnList = new LinkedList<Transform>();
     private LinkedListNode<Transform> nextSpawn;
+
+    public GameObject lastSpawnedBox;
 
     private void Start()
     {
@@ -33,9 +36,50 @@ public class Spawner : MonoBehaviour
 
     private IEnumerator SpawnBox()
     {
-        Instantiate(box, nextSpawn.Value.position + Vector3.up * altimeter.getNextAltitude(), nextSpawn.Value.rotation);
+        // Get next spawn position
+        Vector3 altitudeCorrection = Vector3.up * altimeter.getNextAltitude();
+        Vector3 spawnPosition = nextSpawn.Value.position + altitudeCorrection;
+
+        // Spawn new box
+        GameObject newSpawnedBox = Instantiate(box, spawnPosition, nextSpawn.Value.rotation);
+
+        BoxCollider[] lastSubBoxes = lastSpawnedBox.GetComponentsInChildren<BoxCollider>();
+        if (lastSubBoxes.Length == 2)
+        {
+            GameObject referenceBox = lastSubBoxes[1].gameObject;
+            BoxCollider[] newSubBoxes = newSpawnedBox.GetComponentsInChildren<BoxCollider>();
+            GameObject[] boxesToCorrect = newSubBoxes.Select(subBox => subBox.gameObject).ToArray();
+            CorrectXScale(boxesToCorrect, referenceBox);
+
+            // Correct x start position
+            BoxCollider[] originalSubBoxes = box.GetComponentsInChildren<BoxCollider>();
+            BoxCollider originalBox = originalSubBoxes[0];
+            float originalXSize = originalBox.size.x * originalBox.transform.localScale.x;
+            float actualXSize = newSubBoxes[0].size.x * newSubBoxes[0].transform.localScale.x;
+            Vector3 latitudeCorrection = nextSpawn.Value.right * originalXSize / 2 - nextSpawn.Value.right * actualXSize / 2;
+            Vector3 newSpawnPosition = spawnPosition + latitudeCorrection;
+            newSpawnedBox.transform.position = newSpawnPosition;
+        }
+
+
+        // Set lastBox as newBox
+        lastSpawnedBox = newSpawnedBox;
         nextSpawn = CircularLinkedList.NextOrFirst(nextSpawn);
         yield return null;
+    }
+
+    private void CorrectXScale(GameObject[] boxesToCorrect, GameObject referenceBox)
+    {
+        float newZScale = referenceBox.transform.localScale.z;
+        boxesToCorrect.ToList().ForEach(newSubBox =>
+        {
+            Vector3 newScale = new Vector3(
+                newZScale,
+                newSubBox.transform.localScale.y,
+                newSubBox.transform.localScale.z
+            );
+            newSubBox.transform.localScale = newScale;
+        });
     }
 }
 
